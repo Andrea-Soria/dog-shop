a96fd89a076d1eaea3e50668168b3ce24f25ad93
andrea soria <andreasg16@hotmail.com.ar>
2023-04-19 13:05:07 -0300

    soria galvarro

---
+++ src/Components/CatWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+
+
+const CartWidget = () => {
+  return (
+    <div>
+        <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShA3D9FXsBnu0mWSfW0fMVO_5Vk5LM6Lcwtw&usqp=CAU"} alt="" className= "w-25" />
+        <p style={{color: 'black', fontSize: '20px', marginInlineEnd:'10px'}}>1</p> 
+    </div>
+  )
+}
+
+export default CartWidget
